6bffdc7a7312f1b648d7ddbe840f6b7b76e1e81b
takenncs <116774376+Takennncs@users.noreply.github.com>
2025-10-04 16:33:23 +0300

    Create sidebar.php

---
+++ sidebar.php
@@ -0,0 +1,4 @@
+    <nav class="flex-1 flex flex-col gap-2">
+      <a href="" class="flex items-center gap-2 px-4 py-2 bg-gray-700 rounded"><i class="fas fa-home"></i> Avaleht</a>
+    </nav>
+    <!-- Edasi tee ise nuub -->
