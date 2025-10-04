6a2a536d61cd0021fd75e9aa24b824f46cf85671
takenncs <116774376+Takennncs@users.noreply.github.com>
2025-10-04 16:33:09 +0300

    Create sql.sql

---
+++ sql.sql
@@ -0,0 +1,18 @@
+CREATE TABLE `users` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `uid` varchar(32) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `role` varchar(50) NOT NULL DEFAULT 'Mängija'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `uid` (`uid`),
+  ADD UNIQUE KEY `email` (`email`);
+
+ALTER TABLE `users`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+COMMIT;
